76e7a9b412c38e75e149b2e67ba157ec152e6547
Navneet Kumar Choudhary <132658909+navneetcronj@users.noreply.github.com>
2023-05-22 13:24:01 +0530

    fix: tailwind css fixed

--- Components/Button/buttonComponent.js
+++ Components/Button/buttonComponent.js
@@ -1,17 +1,15 @@
-import React from "react";
+import React from 'react';
 
 const Button = ({ children, onClick, className }) => {
-
-    return (
-        <>
-          <button className={`bg-transparent hover:bg-red-500 text-white-200 font-semibold py-2 px-4 border
-             border-blue-500 hover:border-transparent rounded`} 
-             onClick={onClick}
-            >
-  {children}
-</button>
-        </>
-    )
-
-}
+  return (
+    <>
+      <button
+        className="bg-red-500 text-white-200 font-semibold py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        onClick={onClick}
+      >
+        {children}
+      </button>
+    </>
+  );
+};
 export default Button;
