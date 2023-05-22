e8e4132fb7901c48883027794e2e4c46d8f55af8
Navneet Kumar Choudhary <132658909+navneetcronj@users.noreply.github.com>
2023-05-22 18:35:28 +0530

    Merge branch 'dev' of https://github.com/githubcronj/employer-consultant-frontend into navneet-dev

--- Components/Navbar/Navbar.js
+++ Components/Navbar/Navbar.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 export const Navbar = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <div className="bg-[#FFFFFF] bg-no-repeat bg-origin-padding shadow-[0px_1px_4px_rgba(21,34,50,0.08)] opacity-100 flex flex-row h-[68px] justify-center items-center">
+      <div className="text-[#131523] font-semibold text-left text-[20px] ">
+        Logo
+      </div>
+      <div>right side</div>
+    </div>
   );
 };
