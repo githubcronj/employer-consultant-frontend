1ce5c7e67aa96fc13cc9ded8066e2ce791c54c6a
Navneet Kumar Choudhary <132658909+navneetcronj@users.noreply.github.com>
2023-05-23 12:12:15 +0530

    feat: font family added

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import Login from 'Components/Login/Login';
-import Signup from 'Components/Signup/Signup';
 
 export default function Home() {
-  return (
-    <div>
-      <Login />
-      <Signup />
-    </div>
-  );
+  return <div></div>;
 }
