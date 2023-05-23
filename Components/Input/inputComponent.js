009025de5a9280c572ba9a8af14796bc41ca49da
keerthicronj <keerthi@cronj.com>
2023-05-23 18:41:10 +0530

    added icons

--- Components/Input/inputComponent.js
+++ Components/Input/inputComponent.js
@@ -13,13 +13,13 @@ const InputComponent = ({
   return (
     <div className='relative'>
       {lefticon && (
-        <div className='absolute left-2 top-2  px-2 py-2 ml-2 mt-2 '>
+        <div className='absolute left-2 top-0.5  px-2 py-2 ml-2 mt-2 '>
           <img src={lefticon} alt='lefticon' className='w-5 h-5' />
         </div>
       )}
       <div
         className='border
-         border-gray-400 w-0 h-7 absolute top-5 left-16'
+         border-gray-300 w-0 h-9 absolute top-[10px] left-16'
       ></div>
       <input
         className={`bg-transparent text-black rounded-2xl sm:w-96 font-semibold
@@ -33,7 +33,7 @@ const InputComponent = ({
       />
 
       {righticon && (
-        <div className='absolute top-2 right-2  px-2 py-2 ml-2 mt-2'>
+        <div className='absolute top-0.5 right-2  px-2 py-2 ml-2 mt-2'>
           <img
             src={righticon}
             alt='rightIcon'
