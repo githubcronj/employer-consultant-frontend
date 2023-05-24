d09e707b59859d732b01d205186f714313468daf
keerthicronj <keerthi@cronj.com>
2023-05-24 18:12:01 +0530

    updated

--- store/saga/registerSaga.js
+++ store/saga/registerSaga.js
@@ -2,6 +2,7 @@ import { takeLatest, call, put } from "redux-saga/effects";
 import * as types from "../type/registerType";
 import api from "../../utils/api";
 import { useRouter } from "next/router";
+import { toast } from "react-toastify";
 
 export default function* watchRegister() {
   yield takeLatest(types.REGISTER_REQUEST, register);
@@ -12,8 +13,8 @@ function* register(action) {
     const response = yield call(api.post, "/register", action.payload);
     yield put({ type: types.REGISTER_SUCCESS, payload: response.data });
     yield put(push("/home"));
-          
   } catch (error) {
     yield put({ type: types.REGISTER_ERROR, payload: error.message });
+    toast.error(error.message);
   }
 }
