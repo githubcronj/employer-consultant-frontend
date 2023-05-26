db5cbca61157667a67345eed160f363ea3c94659
saimakhanac <saimakhan0zy0@gmail.com>
2023-05-26 12:44:29 +0530

    New job post page

--- pages/home.js
+++ pages/home.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React from "react";
 
 function createData(
@@ -105,10 +106,10 @@ const tableHeading = [
 ];
 const home = () => {
   return (
-    <div className="flex">
+    <div>
       {/* <div style={{ width: "220px" }}>hello</div> */}
       {/* home */}
-      <div className="flex-1 bg-[#2B373C1C] py-4 px-2 sm:px-4">
+      <div className="bg-[#2B373C1C] py-4 px-2 sm:px-4">
         <div className="bg-white pt-3">
           <div className="flex justify-between items-center mx-5 sm:mx-9 ">
             <div className="my-3 flex gap-4">
@@ -145,9 +146,11 @@ const home = () => {
               <button className="m-3">
                 <img src="/Assets/download.svg" alt="save" />
               </button>
+              <Link href="/newJobPost">
               <button className="m-3">
                 <img src="/Assets/add.svg" alt="save" />
               </button>
+              </Link>
             </div>
           </div>
           {/* table */}
---
+++ pages/newJobPost.js
@@ -0,0 +1,311 @@
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const NewJobPost = () => {
+  const [jobPostData, setJobPostData] = useState({
+    jobTitle: "",
+    experience: "",
+    applicationDeadline: "",
+    jobType: "",
+    hourly: "",
+    monthly: "",
+    yearly: "",
+    min: "",
+    max: "",
+    jobDescription: "",
+    email: "",
+    phoneNumber: "",
+  });
+
+  const handleChange = (e) => {
+    const { id, value } = e.target;
+    setJobPostData((prevValues) => ({
+      ...prevValues,
+      [id]: value,
+    }));
+  };
+
+  return (
+    <div className="bg-[#2B373C1C] py-4 px-2 sm:px-4">
+      <div className="bg-white">
+        <div className="md:flex justify-between items-center mx-5 sm:mx-9 py-1">
+          <div className="my-3 flex gap-6">
+            <Link href="/home">
+              <Image
+                src="/Assets/backbtn.svg"
+                alt="back button"
+                width={35}
+                height={35}
+                className="cursor-pointer"
+              />
+            </Link>
+            <p className="text-lg sm:text-2xl font-bold">Create New Job Post</p>
+          </div>
+          <div className="flex gap-2 sm:gap-5">
+            <button className="px-11 py-3 bg-red-500 text-white rounded-[16px] inline-flex gap-4 items-center tracking-wide uppercase my-3">
+              Save
+            </button>
+            <button className="px-8 py-3 bg-white border border-red-500 text-red-500 rounded-[16px] inline-flex gap-4 items-center tracking-wide uppercase my-3">
+              Cancel
+            </button>
+          </div>
+        </div>
+        {/* form section */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mx-3 lg:mx-20 lg:px-10 xl:px-24 py-8 my-3">
+          <div className="relative">
+            <label
+              className="absolute top-[-8px] left-0 ml-2 mt-px  bg-white px-1 text-[#1E0F3B] text-xs font-bold"
+              for="jobTitle"
+            >
+              Job Title
+            </label>
+            <input
+              type="text"
+              id="jobTitle"
+              placeholder="UX Designer"
+              required
+              className="py-5 px-4 border rounded-[10px] border-[#D8D8DD] w-full"
+              value={jobPostData.jobTitle}
+              onChange={handleChange}
+            />
+          </div>
+          {/*  */}
+          <div>
+            <select
+              id="experience"
+              required
+              className="py-5 px-4 border rounded-[10px] border-[#D8D8DD] w-full custom-select"
+              style={{
+                WebkitAppearance: "none",
+                MozAppearance: "none",
+                appearance: "none",
+                backgroundImage: "none",
+                backgroundImage: "url(/Assets/down-arrow.svg)",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "95% center",
+                paddingRight: "20px",
+              }}
+            >
+              <option value="">Experience</option>
+              <option value="one">1 year</option>
+              <option value="two">2 year</option>
+              <option value="three">3 year</option>
+            </select>
+          </div>
+          {/*  */}
+          
+          <div className="relative flex items-center">
+            <DatePicker
+              id="applicationDeadline"
+              placeholderText="Application Deadline"
+              required
+              className={`block py-5 px-4 w-full text-gray-900 dark:bg-gray-700 border rounded-[10px] border-[#D8D8DD] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer`}
+              selected={jobPostData.applicationDeadline}
+              onChange={(date) =>
+                handleChange({
+                  target: { id: "applicationDeadline", value: date },
+                })
+              }
+            />
+            <label
+              for="founded"
+              className="absolute hidden my-1 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+            >
+              Founded In{" "}
+            </label>
+            <img
+              src="/Assets/calendar.svg"
+              alt="calendar"
+              className="absolute right-2"
+              onClick={() => document.getElementById("founded").click()}
+            />{" "}
+          </div>
+          {/*  */}
+
+          <div>
+            <select
+              id="jobtype"
+              required
+              className="py-5 px-4 border rounded-[10px] border-[#D8D8DD] w-full custom-select"
+              style={{
+                WebkitAppearance: "none",
+                MozAppearance: "none",
+                appearance: "none",
+                backgroundImage: "none",
+                backgroundImage: "url(/Assets/down-arrow.svg)",
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "95% center",
+                paddingRight: "20px",
+              }}
+            >
+              <option value="">Job Type</option>
+              <option value="UI">UX Designer</option>
+              <option value="React">React Developer</option>
+              <option value="Devops">Dev Ops Engineer</option>
+            </select>
+          </div>
+
+          {/*  salary starts here  */}
+          <div className="relative sm:col-span-2">
+            <div
+              className="absolute top-[-8px] left-0 ml-2 mt-px  bg-white px-1 text-[#1E0F3B] text-xs font-bold"
+              for="jobTitle"
+            >
+              Salary
+            </div>
+            <div className="py-5 px-4 border rounded-[10px] border-[#D8D8DD] w-full grid grid-cols-1 md:grid-cols-2">
+              <div className="my-3 md:my-0">
+                <ul className="justify-center md:justify-normal flex gap-3 sm:gap-8 md:gap-3 items-center">
+                  <li>
+                    <div className="flex">
+                      <input type="checkbox" id="hourly" className="peer hidden" />
+                      <label
+                        for="hourly"
+                        className="select-none cursor-pointer rounded-lg bg-gray-200 py-5 text-md sm:text-lg px-3 sm:px-6 text-gray-700 transition-colors duration-200 ease-in-out peer-checked:bg-[#5E9AF8] peer-checked:text-white peer-checked:border-0 "
+                      >
+                        Hourly
+                      </label>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="flex">
+                      <input type="checkbox" id="monthly" class="peer hidden" />
+                      <label
+                        for="monthly"
+                        className="select-none cursor-pointer rounded-lg bg-gray-200 py-5 text-md sm:text-lg px-3 sm:px-5 text-gray-700 transition-colors duration-200 ease-in-out peer-checked:bg-[#5E9AF8] peer-checked:text-white peer-checked:border-0 "
+                      >
+                        Monthly
+                      </label>
+                    </div>
+                  </li>
+                  <li>
+                    <div className="flex">
+                      <input type="checkbox" id="yearly" class="peer hidden" />
+                      <label
+                        for="yearly"
+                        className="select-none cursor-pointer rounded-lg bg-gray-200 py-5 text-md sm:text-lg px-4 sm:px-7 text-gray-700 transition-colors duration-200 ease-in-out peer-checked:bg-[#5E9AF8] peer-checked:text-white peer-checked:border-0 "
+                      >
+                        Yearly
+                      </label>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+              {/* min max */}
+              <div className="flex gap-3 justify-evenly md:justify-normal">
+                {/* min */}
+                <div className="relative">
+                  <input
+                    type="text"
+                    id="min"
+                    placeholder=" "
+                    required
+                    className={`block py-5 px-4 w-full text-sm text-gray-900 dark:bg-gray-700 border rounded-[10px] border-[#D8D8DD] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer
+                `}
+                    value={jobPostData.min}
+                    onChange={handleChange}
+                  />
+
+                  <label
+                    for="min"
+                    className="absolute my-1 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+                  >
+                    Min
+                  </label>
+                </div>
+                {/* max */}
+                <div className="relative">
+                  <input
+                    type="text"
+                    id="max"
+                    placeholder=" "
+                    required
+                    className={`block py-5 px-4 w-full text-sm text-gray-900 dark:bg-gray-700 border rounded-[10px] border-[#D8D8DD] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer
+                        `}
+                    value={jobPostData.max}
+                    onChange={handleChange}
+                  />
+
+                  <label
+                    for="max"
+                    className="absolute my-1 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+                  >
+                    Max
+                  </label>
+                </div>
+              </div>
+              {/* min max ends here */}
+            </div>
+          </div>
+          {/* salary ends here */}
+
+          {/* jd */}
+          <div className="sm:col-span-2 relative">
+            <textarea
+              type="text"
+              id="jobDescription"
+              placeholder=" "
+              required
+              className="block py-5 px-4 w-full text-gray-900 dark:bg-gray-700 border rounded-[10px] border-[#D8D8DD] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              value={jobPostData.jobDescription}
+              onChange={handleChange}
+            />
+            <label
+              for="jobDescription"
+              className="absolute my-1 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+            >
+              Job Description
+            </label>
+          </div>
+          {/* jd ends here */}
+          {/* email */}
+          <div className="relative">
+            <input
+              type="text"
+              id="email"
+              placeholder=" "
+              required
+              className={`block py-5 px-4 w-full text-sm text-gray-900 dark:bg-gray-700 border rounded-[10px] border-[#D8D8DD] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer
+                `}
+              value={jobPostData.email}
+              onChange={handleChange}
+            />
+
+            <label
+              for="email"
+              className="absolute my-1 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+            >
+              Email
+            </label>
+          </div>
+          {/* phone number */}
+          <div className="relative">
+            <input
+              type="text"
+              id="email"
+              placeholder=" "
+              required
+              className={`block py-5 px-4 w-full text-sm text-gray-900 dark:bg-gray-700 border rounded-[10px] border-[#D8D8DD] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer
+                `}
+              value={jobPostData.phoneNumber}
+              onChange={handleChange}
+            />
+
+            <label
+              for="phoneNumber"
+              className="absolute my-1 text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4"
+            >
+              Phone Number
+            </label>
+          </div>
+        </div>
+        {/* form section ends here */}
+      </div>
+    </div>
+  );
+};
+
+export default NewJobPost;
