35b4b4d9ec3671e53a51babfc5b825caa2336be2
navneetcronj <132658909+navneetcronj@users.noreply.github.com>
2023-05-29 19:21:51 +0530

    Revert "Keerthi"

--- store/reducer/index.js
+++ store/reducer/index.js
@@ -3,10 +3,9 @@ import registerReducer from "./registerReducer";
 import { LoginReducer } from "./loginReducer";
 import otpReducer from "./otpReducer";
 import reOtpReducer from "./reOtpReducer";
-import { forgotPasswordReducer } from "./forgotpasReducer";
+import {forgotPasswordReducer} from "./forgotpasReducer";
 import setupReducer from "./setupReducer";
-import { changePasswordReducer } from "./changePasReducer";
-import { facebookReducer } from "./facebookReducer";
+import {changePasswordReducer} from "./changePasReducer"
 const rootReducer = combineReducers({
   LoginReducer,
   registerReducer,
@@ -14,8 +13,7 @@ const rootReducer = combineReducers({
   reOtpReducer,
   forgotPasswordReducer,
   setupReducer,
-  changePasswordReducer,
-  facebookReducer,
+  changePasswordReducer
 });
 
 export default rootReducer;
--- store/reducer/loginReducer.js
+++ store/reducer/loginReducer.js
@@ -1,4 +1,5 @@
 import * as types from '../type/logintype';
+
 const INITIAL_STATE = {
   CurrentUser: null,
   isLoggedIn: false,
@@ -32,8 +33,21 @@ export function LoginReducer(state = INITIAL_STATE, action) {
         ...state,
         isLoggedIn:false,
         isLoggedOut:true,
-      } 
-     
+      }  
+
+      //google login
+   
+
+    case types.GOOGLE_LOGIN_SUCCESS:
+      return {
+        ...state,
+        data: action.payload,
+        isLoggedIn: true,
+        isLoggedOut: false,
+       
+      };
+   
+    
     default:
       return state;
   }
