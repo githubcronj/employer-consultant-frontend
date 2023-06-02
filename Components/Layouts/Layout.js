c4448d48d0f889299e87523f8ca1594720bd4f00
saimakhanac <saimakhan0zy0@gmail.com>
2023-06-02 11:06:10 +0530

    changes

--- Components/Layouts/Layout.js
+++ Components/Layouts/Layout.js
@@ -11,7 +11,7 @@ function Layout(props) {
   const shouldRenderComponents = ![
     '/Login',
     '/register',
-    // '/profile',
+    '/profile',
     '/forgotPassword',
     '/resetPassword',
     '/confirmPassword',
