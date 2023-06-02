4c53ea533b9904645bdcbfe6c701781f10810583
saimakhanac <saimakhan0zy0@gmail.com>
2023-06-02 17:45:01 +0530

    View Consultant

---
+++ pages/viewConsultant.js
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import ViewConsultantDetail from "Components/ViewConsultant";
+import React from "react";
+
+const viewConsultant = () => {
+  return (
+    <>
+      <Box sx={{backgroundColor:'#F3F5F8',py:3, px:3}}>
+        <ViewConsultantDetail />
+      </Box>
+    </>
+  );
+};
+
+export default viewConsultant;
