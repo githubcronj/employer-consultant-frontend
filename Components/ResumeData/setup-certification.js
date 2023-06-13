a963bca619516947d747bc37cadf003a8fff57fa
priynkapatil <priyankapatilp22@gmail.com>
2023-06-13 16:53:49 +0530

    added validation in setdetails , map view profile company Name fix navbar userName api

--- Components/ResumeData/setup-certification.js
+++ Components/ResumeData/setup-certification.js
@@ -1,5 +1,5 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -10,11 +10,15 @@ const SetupCertificate = ({
   infodata,
   handleCertificateremovedata,
 }) => {
+  const [errors, setErrors] = useState({});
+
   function CertificateHandleChange(e) {
     tempCertificate(e);
   }
   const addData = (section) => {
-    handleCertificateAdd(section);
+    const isValid = validateForm();
+    if (isValid) {
+    handleCertificateAdd(section);}
   };
   const removeData = (indexdata) => {
     handleCertificateremovedata(indexdata);
@@ -49,9 +53,42 @@ const SetupCertificate = ({
       },
     });
   };
+
   const handleFileInputChange = (event) => {
     const file = event.target.files[0];
     // Handle the selected file here
+  };
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = {};
+
+    if (!certificationDetails.courseName) {
+      newErrors.courseName = "Course Name is required";
+      isValid = false;
+    }
+
+    if (!certificationDetails.issuingOrganization) {
+      newErrors.issuingOrganization = "Field Name is required";
+      isValid = false;
+    }
+
+    if (!certificationDetails.issueDate) {
+      newErrors.issueDate = "Field is required";
+      isValid = false;
+    }
+
+    if (!certificationDetails.expirationDate) {
+      newErrors.expirationDate = "Field is required";
+      isValid = false;
+    }
+
+    if (!certificationDetails.credentialUrl) {
+      newErrors.credentialUrl = "Field is required";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
   };
   return (
     <div className=" bg-white">
@@ -62,29 +99,39 @@ const SetupCertificate = ({
               type="text"
               id="courseName"
               placeholder="Course Name"
-              className="py-5 px-4 border rounded-[10px] border-[#D8D8DD] w-full"
+              className={`py-5 px-4 border rounded-[10px] ${
+                errors.courseName ? "border-red-500" : "border-[#D8D8DD]"
+              } w-full`}
               name="tempCertificate.courseName"
               value={certificationDetails?.courseName || ""}
               onChange={CertificateHandleChange}
-            />
+            />{errors.courseName && (
+              <p className="text-red-500 text-xs">{errors.courseName}</p>
+            )}
           </div>
           <div>
             <input
               type="text"
               id="issuingOrganization"
               placeholder="Issuing organization"
-              className="py-5 px-4 border rounded-[10px] border-[#D8D8DD] w-full"
+              className={`py-5 px-4 border rounded-[10px] ${
+                errors.issuingOrganization ? "border-red-500" : "border-[#D8D8DD]"
+              } w-full`}
               name="tempCertificate.issuingOrganization"
               value={certificationDetails?.issuingOrganization || ""}
               onChange={CertificateHandleChange}
-            />
+            />{errors.issuingOrganization && (
+              <p className="text-red-500 text-xs">{errors.issuingOrganization}</p>
+            )}
           </div>
           <div>
             <div className="relative flex items-center">
               <DatePicker
                 id="issueDate"
                 placeholderText="Issue date"
-                className="block py-5 px-4 w-full text-gray-900 dark:bg-gray-700 border rounded-[10px] border-[#D8D8DD] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                className={`block py-5 px-4 w-full text-gray-900 border rounded-[10px] ${
+                errors.issueDate ? "border-red-500" : "border-[#D8D8DD]"
+              } border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer `}
                 name={tempCertificate.issueDate}
                 selected={
                   certificationDetails?.issueDate
@@ -99,7 +146,9 @@ const SetupCertificate = ({
                 className="absolute right-2"
                 onClick={() => document.getElementById("issueDate").click()}
               />
-            </div>
+            </div>{errors.issueDate && (
+              <p className="text-red-500 text-xs">{errors.issueDate}</p>
+            )}
           </div>
 
           <div>
@@ -107,7 +156,9 @@ const SetupCertificate = ({
               <DatePicker
                 id="expirationDate"
                 placeholderText="Expiration date"
-                className="block py-5 px-4 w-full text-gray-900 dark:bg-gray-700 border rounded-[10px] border-[#D8D8DD] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                className={`block py-5 px-4 w-full text-gray-900 border rounded-[10px]  border-gray-300 appearance-none  focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                errors.expirationDate ? "border-red-500" : "border-[#D8D8DD]"
+              }`}
                 name={tempCertificate.expirationDate}
                 selected={
                   certificationDetails?.expirationDate
@@ -124,7 +175,9 @@ const SetupCertificate = ({
                   document.getElementById("expirationDate").click()
                 }
               />
-            </div>
+            </div>{errors.expirationDate && (
+              <p className="text-red-500 text-xs">{errors.expirationDate}</p>
+            )}
           </div>
 
           <div className="sm:col-span-2 ">
@@ -132,18 +185,22 @@ const SetupCertificate = ({
               type="text"
               id="credentialUrl"
               placeholder="Credential URL"
-              className="block py-5 px-4 w-full text-gray-900 dark:bg-gray-700 border rounded-[10px] border-[#D8D8DD] border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              className={`block py-5 px-4 w-full text-gray-900 border rounded-[10px] border-[#D8D8DD] border-gray-300 appearance-none   focus:outline-none focus:ring-0 focus:border-blue-600 peer ${
+                errors.expirationDate ? "border-red-500" : "border-[#D8D8DD]"
+              }`}
               name="tempCertificate.credentialUrl"
               value={certificationDetails?.credentialUrl || ""}
               onChange={CertificateHandleChange}
-            />
+            />{errors.credentialUrl && (
+              <p className="text-red-500 text-xs">{errors.credentialUrl}</p>
+            )}
           </div>
           <div className="sm:col-span-2 ">
             <div>
               <input
                 id="image"
                 type="file"
-                name="personalDetails.image"
+                // name="personalDetails.image"
                 accept=".jpg,.jpeg,.png,.svg"
                 style={{ display: "none" }}
                 onChange={handleFileInputChange}
--- Components/ResumeData/setup-education.js
+++ Components/ResumeData/setup-education.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SetupEducation = ({
   educationDetails,
@@ -7,16 +7,51 @@ const SetupEducation = ({
   infodata,
   handleEducationremovedata,
 }) => {
+  const [errors, setErrors] = useState({});
+
   function eduHandleChage(e) {
     tempeducation(e);
   }
 
-  const addData = (section) => {
-    handleEducationAdd(section);
+  const addData = () => {
+    const isValid = validateForm();
+    if (isValid) {
+      handleEducationAdd();
+    }
   };
+
   const removeData = (indexdata) => {
     handleEducationremovedata(indexdata);
   };
+
+  const validateForm = () => {
+    let isValid = true;
+    const newErrors = {};
+
+    if (!educationDetails.level) {
+      newErrors.level = "Highest Education Level is required";
+      isValid = false;
+    }
+
+    if (!educationDetails.institutionName) {
+      newErrors.institutionName = "Institution Name is required";
+      isValid = false;
+    }
+
+    if (!educationDetails.degreeName) {
+      newErrors.degreeName = "Degree/Diploma is required";
+      isValid = false;
+    }
+
+    if (!educationDetails.year) {
+      newErrors.year = "Year of Passing is required";
+      isValid = false;
+    }
+
+    setErrors(newErrors);
+    return isValid;
+  };
+
   return (
     <div className=" bg-white ">
       <form onSubmit={(e) => e.preventDefault()}>
@@ -25,7 +60,9 @@ const SetupEducation = ({
             <select
               id="level"
               required
-              className="py-5 px-4 border rounded-[10px] border-[#D8D8DD] w-full custom-select"
+              className={`py-5 px-4 border rounded-[10px] ${
+                errors.level ? "border-red-500" : "border-[#D8D8DD]"
+              } w-full custom-select`}
               style={{
                 WebkitAppearance: "none",
                 MozAppearance: "none",
@@ -44,7 +81,9 @@ const SetupEducation = ({
               <option value="BCA">BCA</option>
               <option value="BE">BE</option>
               <option value="B.Tech">B.Tech</option>
-            </select>
+            </select> {errors.level && (
+              <p className="text-red-500 text-xs">{errors.level}</p>
+            )}
           </div>
           <div>
             <input
@@ -52,17 +91,23 @@ const SetupEducation = ({
               id="institutionName"
               placeholder="Institution Name"
               required
-              className="py-5 px-4 border rounded-[10px] border-[#D8D8DD] w-full"
+              className={`py-5 px-4 border rounded-[10px]  w-full ${
+                errors.institutionName ? "border-red-500" : "border-[#D8D8DD]"
+              } `}
               name="tempeducation.institutionName"
               value={educationDetails.institutionName || ""}
               onChange={eduHandleChage}
-            />{" "}
+            />{errors.institutionName && (
+              <p className="text-red-500 text-xs">{errors.institutionName}</p>
+            )}
           </div>
           <div>
             <select
               id="degreeName"
               required
-              className="py-5 px-4 border rounded-[10px] border-[#D8D8DD] w-full custom-select"
+              className={`py-5 px-4 border rounded-[10px] ${
+                errors.degreeName ? "border-red-500" : "border-[#D8D8DD]"
+              } w-full custom-select `}
               style={{
                 WebkitAppearance: "none",
                 MozAppearance: "none",
@@ -82,7 +127,9 @@ const SetupEducation = ({
               <option value="Associate degree">Associate degree</option>
               <option value="Bachelor degree">Bachelor degree</option>
               <option value="Master degree">Master degree</option>
-            </select>
+            </select>{errors.degreeName && (
+              <p className="text-red-500 text-xs">{errors.degreeName}</p>
+            )}
           </div>
           <div>
             <input
@@ -90,11 +137,15 @@ const SetupEducation = ({
               id="year"
               placeholder="Year of Passing"
               required
-              className="py-5 px-4 border rounded-[10px] border-[#D8D8DD] w-full"
+              className={`py-5 px-4 border rounded-[10px]  w-full ${
+                errors.year ? "border-red-500" : "border-[#D8D8DD]"
+              } `}
               name="tempeducation.year"
               value={educationDetails.year || ""}
               onChange={eduHandleChage}
-            />
+            />{errors.year && (
+              <p className="text-red-500 text-xs">{errors.year}</p>
+            )}
           </div>
         </div>
         <div className="flex justify-end">
